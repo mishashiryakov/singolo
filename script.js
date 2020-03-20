b52abcd9252dcfb86e0b5e9eda7a7438552ebe5e
mishashiryakov <mishashiryakov@gmail.com>
2020-03-20 07:08:20 +0300

    feat: add scroll anim

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-const MENU = document.getElementById('menu');
+// const MENU = document.getElementById('menu');
 
 
-MENU.addEventListener('click', (event) => {
-    MENU.querySelectorAll('a').forEach(el => el.classList.remove('selected'));
+// MENU.addEventListener('click', (event) => {   //переключение активного класса для нав бара
+//     MENU.querySelectorAll('a').forEach(el => el.classList.remove('selected'));
 
-    event.target.classList.add('selected');
-})
+//     event.target.classList.add('selected');
+// })
+
+
+
 
 
 let items = document.querySelectorAll('.slider .item');
 let currentItem = 0;
 let isEnabled = true;
 
-function changeCurrentItem(n) {
+function changeCurrentItem(n) {   // slider
 	currentItem = (n + items.length) % items.length;
 }
 
@@ -56,3 +59,27 @@ document.querySelector('.forward').addEventListener('click', function() {
 		nextItem(currentItem);
 	}
 });
+
+
+
+document.addEventListener('scroll', onScroll);    //делаем плавный скрол и якоря 
+
+function onScroll(event) {
+
+	const curPos = window.scrollY;
+	const divs = document.querySelectorAll('.wrapper>div'); 
+	const links = document.querySelectorAll('#menu a');
+	
+	divs.forEach((el) => {
+		console.log(el.offsetTop);
+		if(el.offsetTop <= curPos  && (el.offsetTop + el.offsetHeight) > curPos) {
+			links.forEach((a) => {
+				a.classList.remove('selected');
+				if(el.getAttribute('id') === a.getAttribute('href').substring(1)) {
+					a.classList.add('selected');
+				}
+			})
+		}
+	})
+
+}
